6514f1a82bc820f97850a002bb72feef4cb85994
StanGad <147396045+StanGad@users.noreply.github.com>
2024-02-02 11:25:31 +0100

    Update CodeOK_Gant.ino

--- CodeOK_Gant.ino
+++ CodeOK_Gant.ino
@@ -1,15 +1,15 @@
-// Entrée analogique à utiliser (entre 0 et 5 pour un Arduino UNO)
+
 int entreeAnalogique = 2;
 int entreeAnalogique2 = 14;
 
  
-// Valeur de la résistance de mesure R1 en ohms
+
 #define R1 430.0F
  
 void setup()
 {
 pinMode(5, OUTPUT);
-// Initialisation de la communication série
+
 Serial.begin(115200);
 }
  
@@ -21,7 +21,7 @@ void loop()
 float valeurLue = (float)analogRead(entreeAnalogique);
 float valeurLue2 = (float)analogRead(entreeAnalogique2);
  
-// calcul de la valeur de la résistance en ohms
+
 float resistanceLue = (valeurLue * R1) / (1023.0F - valeurLue);
 float resistanceLue2 = (valeurLue2 * R1) / (1023.0F - valeurLue2);
 
@@ -40,7 +40,7 @@ if(abs(resistanceLue2) <810 && abs(resistanceLue) > 950){
 }
  
  
-// affichage de la valeur de la résistance dans la console série
+
 Serial.print("Resistance 1: ");
 Serial.print(abs(resistanceLue));
 Serial.println("Ohms");
@@ -51,7 +51,6 @@ Serial.println("Ohms");
 
 
 
- 
-// attendre 1 seconde avant la prochaine mesure
+
 delay(250);
 }
